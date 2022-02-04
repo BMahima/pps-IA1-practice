982e748392c92048106ea5ac5072bb106564059c
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-02-04 03:48:41 +0000

    p1 done

---
+++ p1final.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void input(int *a, int *b)
+{
+  printf("enter the two numbers: ");
+  scanf("%d,%d \n",a,b);
+}
+void add(int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+void output(int a, int b, int sum)
+{
+  printf("%d+%d=%d \n",a,b,sum);
+}
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
+  return 0;
+}
